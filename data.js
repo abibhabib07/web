0659d0c6b7155cd3d90f2ac495588deb5a548a9b
ab1bbb <abibbbhabib@gmail.com>
2026-04-24 11:20:39 +0700

    Update data.js

--- data.js
+++ data.js
@@ -2,10 +2,21 @@ const databaseProyek = {
     "sistem_post": {
         judul: "Sistem post untuk PT.CICAKcicakDIdinding",
         keahlian: ["HTML", "CSS", "JavaScript"],
-        gambar: "https://assets.pikiran-rakyat.com/crop/0x0:0x0/750x500/photo/2022/09/03/2952325585.jpg",
+        gambar: "https://fumida.co.id/wp-content/uploads/2020/11/cicak-2.jpg",
         latar: "ini adalah saya yang sedang mengerjakan proyek sistem post untuk PT.CICAKcicakDIdinding",
         tantangan: "Tantangan yang dihadapi adalah... mencari solusi untuk mengelola data dengan efisien.",
         pelajaran: "Saya belajar tentang pentingnya perencanaan dan pengujian dalam pengembangan perangkat lunak.",
         link: ""
     },
+
+   "sistem_informasi": {
+       judul: "Sistem post untuk PT.MANTAP",
+       keahlian: ["HTML", "CSS", "JavaScript", "PHP", "MySQL"],
+       gambar: "https://images.glints.com/unsafe/160x0/glints-dashboard.oss-ap-southeast-1-internal.aliyuncs.com/company-logo/57676a306ded60177ee7c5a7f9684f5f.png",
+       latar: "Ini adalah proyek sistem post untuk PT.MANTAP yang saya kerjakan.",
+       tantangan: "Tantangan yang dihadapi adalah... mengintegrasikan berbagai sistem dan memastikan keamanan data.",
+       pelajaran: "Saya belajar tentang pentingnya keamanan data dan pengelolaan basis data.",
+       link: ""
+   }
+
 };
