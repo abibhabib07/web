61e09b778c895ec7625033197f2a2e2130beb9ab
ab1bbb <abibbbhabib@gmail.com>
2026-04-24 09:22:45 +0700

    Upload Github pertamaku

---
+++ data.js
@@ -0,0 +1,11 @@
+const databaseProyek = {
+    "sistem_post": {
+        judul: "Sistem post untuk PT.CICAKcicakDIdinding",
+        keahlian: ["HTML", "CSS", "JavaScript"],
+        gambar: "https://assets.pikiran-rakyat.com/crop/0x0:0x0/750x500/photo/2022/09/03/2952325585.jpg",
+        latar: "ini adalah saya yang sedang mengerjakan proyek sistem post untuk PT.CICAKcicakDIdinding",
+        tantangan: "Tantangan yang dihadapi adalah... mencari solusi untuk mengelola data dengan efisien.",
+        pelajaran: "Saya belajar tentang pentingnya perencanaan dan pengujian dalam pengembangan perangkat lunak.",
+        link: ""
+    },
+};
